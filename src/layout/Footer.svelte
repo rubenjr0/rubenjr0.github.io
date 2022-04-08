f2f7f2481de1faebc0b06843ee58263f4c90037d
Rubén <rubenjr0@protonmail.com>
2022-04-08 02:25:14 +0200

    🎨 Footer github link

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -1,5 +1,10 @@
 <section class="footer">
-    Made with <a href="http://svelte.dev" target="_blank">Svelte</a>, and that's pretty much it 👌
+    <p>
+        Made with <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a>, and that's pretty much it 👌
+    </p>
+    <p>
+        The code for this website is availabe on my <a href="https://github.com/rubenjr0/rubenjr0.github.io" target="_blank">Github</a>.
+    </p>
 </section>
 
 <style>
