a2988f235d70d11d7bf7f6750ce5e91b59d2538a
Rubén <rubenjr0@protonmail.com>
2022-04-08 17:45:03 +0200

    🎨 CSS variables on the footer, plus style fixes

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -2,14 +2,9 @@
 	<p>
 		<small>
 			Made with <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a>.
-		</small>
-	</p>
-	<p>
-		<small>
-			The code for this website is availabe on my <a
-				href="https://github.com/rubenjr0/rubenjr0.github.io"
-				target="_blank">Github</a
-			>.
+			<br />
+			The code for this website is availabe on my
+			<a href="https://github.com/rubenjr0/rubenjr0.github.io" target="_blank">Github</a>.
 		</small>
 	</p>
 </section>
@@ -17,7 +12,7 @@
 <style>
 	.footer {
 		padding: 2em 2em;
-		background: #0e0e1b;
+		background: var(--bg-light);
 		text-align: center;
 	}
 </style>
