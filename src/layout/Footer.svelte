e3e6925a5a8fa02fcda5329860f580ec18339d33
Rubén <rubenjr0@protonmail.com>
2022-04-08 03:09:56 +0200

    🎨 Improved footer

--- src/layout/Footer.svelte
+++ src/layout/Footer.svelte
@@ -1,16 +1,24 @@
 <section class="footer">
-    <p>
-        Made with <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a>, and that's pretty much it 👌
-    </p>
-    <p>
-        The code for this website is availabe on my <a href="https://github.com/rubenjr0/rubenjr0.github.io" target="_blank">Github</a>.
-    </p>
+	<p>
+		<small>
+			Made with <a href="https://kit.svelte.dev/" target="_blank">SvelteKit</a>, and that's pretty
+			much it 👌
+		</small>
+	</p>
+	<p>
+		<small>
+			The code for this website is availabe on my <a
+				href="https://github.com/rubenjr0/rubenjr0.github.io"
+				target="_blank">Github</a
+			>.
+		</small>
+	</p>
 </section>
 
 <style>
-    .footer {
-        padding: 2em 2em;
-        background: #0E0E1B;
-        text-align: center;
-    }
+	.footer {
+		padding: 2em 2em;
+		background: #0e0e1b;
+		text-align: center;
+	}
 </style>
